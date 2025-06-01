4fa97f5d25f7c37acf9e6fc3e0fbc07470bec2d1
Yoiq S Rambadian <yoiqrambadian@gmail.com>
2025-06-01 19:05:33 +0700

    :sparkles: Add strategy evaluation pipeline for bollinger bands

---
+++ packages/pipeline/src/pipeline/strategy/daily.py
@@ -0,0 +1,71 @@
+import json
+import pandas as pd
+from argparse import Namespace
+from datetime import datetime, timedelta
+from peewee import Database
+from tqdm import tqdm
+
+from database import (
+    Stock,
+    StrategyEvaluation,
+    connect_database, enable_debug
+)
+
+from pipeline.market.daily import extract_stock_watchlist
+
+from .init import Strategy
+from .utils.data import get_stock_daily_data
+from .utils.bollinger import calculate_bollinger_bands
+
+
+def _calculate_change(series: pd.Series, lag: int = 1) -> float:
+    return series.iloc[-1] / series.iloc[-1 - lag] - 1
+
+
+def evaluate_bollinger_bands(
+        stock: Stock,
+        window: int = 20,
+        *,
+        database: Database
+        ) -> dict:
+    data = get_stock_daily_data(
+        stock.code,
+        start_date=datetime.now() - timedelta(days=60)
+    )
+    bollinger_bands = calculate_bollinger_bands(data, window)
+    current_bb = bollinger_bands.iloc[-1]
+
+    evaluation = {
+        'window': window,
+        'signal': current_bb['signal'],
+        'current_price': data['close'].iloc[-1],
+        'upper_band': current_bb['upper_band'],
+        'lower_band': current_bb['lower_band'],
+        'bandwidth': current_bb['bandwidth'],
+        'bandwidth_change': _calculate_change(
+            bollinger_bands['bandwidth'],
+            window
+        ),
+    }
+
+    with database.atomic():
+        StrategyEvaluation.get_or_create(
+            id=Strategy.BOLLINGER_BANDS,
+            stock=stock,
+            date=current_bb.index.dt.strftime('%Y-%m-%d'),
+            evaluation=json.dumps(evaluation)
+        )
+
+    return evaluation
+
+
+def run_daily_strategy_pipeline(args: Namespace):
+    if args.debug:
+        enable_debug()
+
+    db = connect_database()
+
+    username = 'default'
+    stocks = extract_stock_watchlist(username)
+    for stock in tqdm(stocks):
+        evaluate_bollinger_bands(stock, database=db)
---
+++ packages/pipeline/src/pipeline/strategy/utils/bollinger.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import ta
+
+
+def _evaluate_signal(row: pd.Series) -> str:
+    if row['close'] < row['bollinger_lower_band']:
+        return 'Buy'
+    elif row['close'] > row['bollinger_upper_band']:
+        return 'Sell'
+    return 'Hold'
+
+
+def calculate_bollinger_bands(
+        data: pd.DataFrame,
+        window: int = 20
+        ):
+    data = data[['close']].sort_index()
+
+    bb = ta.volatility.BollingerBands(
+        close=data['close'],
+        window=window
+    )
+    data['upper_band'] = bb.bollinger_hband()
+    data['lower_band'] = bb.bollinger_lband()
+    data['bandwidth'] = data['upper_band'] - data['lower_band']
+    data['signal'] = data.apply(_evaluate_signal, axis=1)
+    data = data.drop('close', axis=1)
+    return data
